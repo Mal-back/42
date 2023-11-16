2f17e44087e1d27c7009c7beb8c8a341308388e5
Valentin Levy <vlevy@bess-f3r2s4.clusters.42paris.fr>
2023-11-16 19:30:14 +0100

    gnl looooool

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlevy <vlevy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 19:03:58 by vlevy             #+#    #+#             */
+/*   Updated: 2023/11/16 19:29:45 by vlevy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+#endif
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
